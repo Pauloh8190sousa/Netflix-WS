421fa24340c37f0c0165ea63b9eef945ce751546
Débora Rodrigues <paulohenrique8190@gmail.com>
2021-09-14 12:02:04 -0300

    Criação de servidor com express na porta 3000 e criação de end-pont

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
+
 const express = require("express");
-const api = express.application;
+const morgan = require("morgan");
+const app = express();
 
-api.get("/");
+app.use(morgan("dev"));
 
-api.listen(3000);
+app.get("/", (req, res) => {
+    res.json({ mensagem: "PEGAR TODOS OS REGISTROS" });
+    //Código do back-end
+});
+
+app.get("/:id", (req, res) => {
+    const id = req.params.id;
+    res.json({ mensagem: `PEGAR TODOS OS REGISTROS COM ID: ${id}` });
+});
+
+app.post("/", (req, res) => {
+    const body = req.body;
+    res.json(body);
+});
+
+app.put("/:id", (req, res) => {
+    const id = req.params.id;
+    res.json({ mensagem: `ATUALIZAR TODOS OS REGISTROS COM ID: ${id}` });
+});
+
+app.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    res.json({ mensagem: `DELETAR TODOS OS REGISTROS COM ID: ${id}` });
+});
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Meu servidor está funcionando");
+});
