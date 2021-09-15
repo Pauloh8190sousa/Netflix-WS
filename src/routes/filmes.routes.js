e9362becaef5ff1e5b5e089f32450852a9d7e83a
Débora Rodrigues <paulohenrique8190@gmail.com>
2021-09-15 11:27:43 -0300

    CRUD completo e requisições ao banco mongoDB funcionando

--- src/routes/filmes.routes.js
+++ src/routes/filmes.routes.js
@@ -2,35 +2,70 @@ const express = require("express");
 const router = express.Router();
 const Filme = require("../models/filme");
 
-router.get("/", (req, res) => {
-    res.json({ mensagem: "PEGAR TODOS OS REGISTROS" });
-    //Código do back-end
+router.get("/", async (req, res) => {
+    try{
+
+        const filmes = await Filme.find({});
+        res.json({ error: false, filmes });
+        
+    }catch(err) {
+        res.json({ error: true, mensagem: err.message  })
+    }
+    
 });
 
-router.get("/:id", (req, res) => {
-    const id = req.params.id;
-    res.json({ mensagem: `PEGAR TODOS OS REGISTROS COM ID: ${id}` });
+router.get("/:id", async (req, res) => {
+    try{
+
+        const id = req.params.id;
+        const filme = await Filme.findById(id);
+        res.json({ error: false, filme });
+
+    }catch(err) {
+        res.json({ error: true, mensagem: err.message  })
+    }
+    
 });
 
 router.post("/", async (req, res) => {
     try {
+
         const filme = req.body;
         const response = await new Filme(filme).save();
         res.json({ error: false, filme: response });
+
     } catch (err) {
         res.json({ error: true, mensagem: err.message });
+        
     }
 
 });
 
-router.put("/:id", (req, res) => {
-    const id = req.params.id;
-    res.json({ mensagem: `ATUALIZAR TODOS OS REGISTROS COM ID: ${id}` });
+router.put("/:id", async (req, res) => {
+    try{
+
+        const id = req.params.id;
+        const filmeUpdate = req.body;
+        const filme = await Filme.findByIdAndUpdate(id, filmeUpdate);
+        res.json({ error: false, filme });
+
+    }catch(err) {
+        res.json({ error: true, mensagem: err.message });
+    }
+    
 });
 
-router.delete("/:id", (req, res) => {
-    const id = req.params.id;
-    res.json({ mensagem: `DELETAR TODOS OS REGISTROS COM ID: ${id}` });
+router.delete("/:id", async (req, res) => {
+    try {
+
+        const id = req.params.id;
+        const deleted = await Filme.findByIdAndDelete(id);
+        res.json({ error: false, deleted });
+
+    }catch(err) {
+        res.json({ error: true, mensagem: err.message });
+    }
+    
 });
 
 
